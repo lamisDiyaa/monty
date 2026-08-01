c87bf1b7a6356f6be8bbd7e74c2b774ece5d586b
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-01 04:51:19 -0700

    Implement mul opcode

--- monty.h
+++ monty.h
@@ -58,4 +58,5 @@ void f_add(stack_t **head, unsigned int counter);
 void f_nop(stack_t **head, unsigned int counter);
 void f_sub(stack_t **head, unsigned int counter);
 void f_div(stack_t **head, unsigned int counter);
+void f_mul(stack_t **head, unsigned int counter);
 #endif /* MONTY_H */
--- opcodes.c
+++ opcodes.c
@@ -266,3 +266,34 @@ void f_div(stack_t **head, unsigned int counter)
 	*head = aux->next;
 	free(aux);
 }
+/**
+ * f_mul - multiplies the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_mul(stack_t **head, unsigned int counter)
+{
+	stack_t *aux;
+	int len = 0, res;
+
+	aux = *head;
+	while (aux)
+	{
+		aux = aux->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = *head;
+	res = aux->next->n * aux->n;
+	aux->next->n = res;
+	*head = aux->next;
+	free(aux);
+}
