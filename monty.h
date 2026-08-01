4dfd5eca5af763cb1347f20f8d49cdcfb56e6d85
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-01 04:41:20 -0700

    Implement nop opcode

--- monty.h
+++ monty.h
@@ -55,4 +55,5 @@ void f_pint(stack_t **head, unsigned int counter);
 void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
 void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
 #endif /* MONTY_H */
