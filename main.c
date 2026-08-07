7016f38d8587d29e06f1fcd081993db94fb31b1e
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-07 04:24:10 -0700

    Implement pchar opcode

--- main.c
+++ main.c
@@ -26,6 +26,7 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 		{"div", f_div},
 		{"mul", f_mul},
 		{"mod", f_mod},
+		{"pchar", f_pchar},
 		{NULL, NULL}
 	};
 	unsigned int i = 0;
--- monty.h
+++ monty.h
@@ -60,5 +60,5 @@ void f_sub(stack_t **head, unsigned int counter);
 void f_div(stack_t **head, unsigned int counter);
 void f_mul(stack_t **head, unsigned int counter);
 void f_mod(stack_t **head, unsigned int counter);
-
+void f_pchar(stack_t **head, unsigned int counter);
 #endif /* MONTY_H */
