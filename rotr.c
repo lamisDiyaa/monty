80f17b8b5bf4412128173fdf8385fbacf65e178d
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-07 06:18:25 -0700

    Fix rotr execution in main and rotr logic

--- rotr.c
+++ rotr.c
@@ -11,17 +11,15 @@ void f_rotr(stack_t **head, unsigned int counter)
 	stack_t *copy;
 	(void)counter;
 
+	copy = *head;
 	if (*head == NULL || (*head)->next == NULL)
 	{
 		return;
 	}
-
-	copy = *head;
 	while (copy->next)
 	{
 		copy = copy->next;
 	}
-
 	copy->next = *head;
 	copy->prev->next = NULL;
 	copy->prev = NULL;
