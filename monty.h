e43af305345f99350fbeeb8792e6e2fdd60bdbbd
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-07 05:08:23 -0700

    Implement rotl opcode

--- monty.h
+++ monty.h
@@ -62,4 +62,5 @@ void f_mul(stack_t **head, unsigned int counter);
 void f_mod(stack_t **head, unsigned int counter);
 void f_pchar(stack_t **head, unsigned int counter);
 void f_pstr(stack_t **head, unsigned int counter);
+void f_rotl(stack_t **head, unsigned int counter);
 #endif /* MONTY_H */
